3dc48c1e28fb247a8fc65cc1e9c7266fe1fba18a
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:14 +0000

    bulk replace system.emit calls

--- christiewd.js
+++ christiewd.js
@@ -201,7 +201,7 @@ instance.prototype.destroy = function() {
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'command': {
 			label:'WD Command',
